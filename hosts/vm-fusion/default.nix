0ecd3dcc18cdea8a11c39c4c8e2b0f155491985b
river <river@macs-MacBook-Pro.local>
2026-02-17 22:24:27 +0700

    update vm fusion

--- hosts/vm-fusion/default.nix
+++ hosts/vm-fusion/default.nix
@@ -18,6 +18,15 @@
   # VMware Fusion guest tools
   virtualisation.vmware.guest.enable = true;
 
+  # GPU/Graphics - bắt buộc cho Wayland compositor (niri)
+  hardware.graphics.enable = true;
+  boot.initrd.kernelModules = [ "virtio_gpu" "vmwgfx" ];
+
+  # Env cho niri chạy trong VM
+  environment.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
+  };
+
   # Networking
   networking.networkmanager.enable = true;
 }
